efcdfeacf2b2ef67c3e684cb931b80d3dadb303d
Jakob <42341254+JLMadsen@users.noreply.github.com>
2020-10-28 14:27:25 +0100

    Update agent.py

--- dqn/agent.py
+++ dqn/agent.py
@@ -18,28 +18,36 @@ class DQN(nn.Module):
         
         self.env = env
         
+        # learning rate
         self.alpha = .001
+        
+        # discount
         self.gamma = .9
+        
+        # exploration rate
         self.upper_epsilon = 1
         self.lower_epsilon = .01
+        self.epsilon = self.upper_epsilon
         
         self.memory = Memory()
         
-        # replace with correct layers
         self.model = nn.Sequential(
-            nn.conv3d(),
+            nn.conv3d(10*20*2, ),
             nn.ReLU(),
             #nn.Conv2d(32, 64, kernel_size=4, stride=2),
             nn.ReLU(),
             #nn.Conv2d(64, 64, kernel_size=3, stride=1),
             nn.ReLU(),
-            #nn.Linear(self.feature_size(), feature_dimension),
+            nn.Linear(env.action_space, 1),
             nn.ReLU(),
         )
+        
+        self.optimizer = optim.Adam(self.parameters, self.alpha)
         
     def forward(self, x):
         return self.model(x)
     
-    def train(self):
+    def policy(self):
         pass
+    
     
