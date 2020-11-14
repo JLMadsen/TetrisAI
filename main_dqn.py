59a46c22d7d488be22dc996436c08009d814145b
Jakob <42341254+JLMadsen@users.noreply.github.com>
2020-11-14 10:01:43 +0100

    benchmark weights

--- main_dqn.py
+++ main_dqn.py
@@ -73,47 +73,52 @@ def train(plot=0, epoch=60_000):
         plt.plot([*range(len(scores))], scores)
         plt.show()
 
-def run(weight=''):
+def run(weight='', attempts=2000):
     print(header('Run trained model'))
     scores = []
     agent.load_weights(weight)
     agent.epsilon = -1
     try:
         while 1:
+            if attempts == env.attempt:
+                break
+            
             state, reward, done, info = env.reset()
             score = 0
             while not done:
                 action = agent.policy(state)
                 state, reward, done, info = env.step(action)
-                print(warning(env.actionName(action)))
+                #print(warning(env.actionName(action)))
                 if reward:
+                    score += reward
                     print(green('CLEARED LINE'))
-                env.render()
-                time.sleep(0.001)
+                #env.render()
+                #time.sleep(0.001)
             print(fail('RESET'))
-            if score:
-                scores.append(score)
+            scores.append(score)
     # break loop on CTRL C or quit pygame window
     except KeyboardInterrupt:
         pass
     except SystemExit:
         pass
             
-    if plot and scores:
-        plt.plot([*range(len(scores))], scores)
+    if scores:
+        plt.plot([*range(len(scores))], scores, label='score')
+        plt.legend()
+        plt.text(0.2, .94, 'Average score = '+ str(sum(scores)/len(scores)), fontsize=12, transform=plt.gcf().transFigure)
         plt.show()
 
 if __name__ == "__main__":
     plot = 0
-    epoch = 60_000
+    epoch = 100_000
     
     try:
         
-        agent.load_weights('_60k_2')
-        agent.upper_epsilon = agent.epsilon = .5
-        train(plot, epoch) # 7:23
+        #agent.load_weights('_60k_2')
+        #agent.upper_epsilon = agent.epsilon = .5
+        #train(plot, epoch) # 7:23
         
-        #run('_60k_2')
+        run('_60k_2')
         
     except KeyboardInterrupt:
         agent.save_weights('_quit')
