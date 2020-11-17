d6d2f1dc9a4a362a630a076f241cb70fb63f61da
Jakob <42341254+JLMadsen@users.noreply.github.com>
2020-11-17 21:19:48 +0100

    benchmark

--- benchmark.py
+++ benchmark.py
@@ -1,13 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+from copy import deepcopy
 
-from enviorment.colors import green, fail, header, cyan, warning
 from enviorment.tetris import Tetris
 
-from nat_selection.agent import Agent as NatAgent
 from nat_selection.model import Model
 
 from dqn.agent import DQN
+
+plt_colors = ['r', 'g', 'b']
+plt_light_colors = ['pink', 'palegreen', 'powderblue']
+
+class randomAgent:
+    def __init__(self, env):
+        self.env = env
+    def policy(self, *args):
+        return self.env.action_sample
 
 def main():
     env = Tetris({'reduced_shapes': 1, 'reduced_grid': 1})
@@ -18,14 +27,31 @@ def main():
     
     agent2 = Model([-0.8995652940240592, 0.06425443268253492, -0.3175211096545741, -0.292974392382306])
 
-    agents = [agent1, agent2]
-    agent_scores = []
+    agent3 = randomAgent(env)
+
+    agent_labels = ['Imitation + DQN', 'Natural Selection', 'Random']
+    agents = [agent1, agent2, agent3]
+    agent_scores = {}
+    sample = 5
+    
+    agents = [deepcopy(agents) for _ in range(sample)]
+    agents = [a for n in agents for a in n]
+    
+    """for a in agents:
+        print(a.__class__.__name__)
+    exit()"""
     
     for agent in agents:
+        current_agent = agent.__class__.__name__
         
-        max_actions = 500
+        max_actions = 2000
         actions = 0
-        agent_scores.append([])
+        #random.seed(420)
+        #np.random.seed(420)
+        if current_agent in agent_scores.keys():
+            agent_scores[current_agent].append([])
+        else:
+            agent_scores[current_agent] = [[]]
         
         while 1:
             
@@ -39,29 +65,39 @@ def main():
                 if actions >= max_actions:
                     break
                 
-                if isinstance(agent, DQN):
-                    action = agent.policy(state)
-                else:
+                if isinstance(agent, Model):
                     action = agent.best(env)
+                else:
+                    action = agent.policy(state)
                     
                 if isinstance(action, list):
                     for a in action:
                         state, reward, done, info = env.step(a)
-                        agent_scores[-1].append(reward)
+                        agent_scores[current_agent][-1].append(reward)
                         actions+=1
                 else:
                     state, reward, done, info = env.step(action)
-                    agent_scores[-1].append(reward)
+                    agent_scores[current_agent][-1].append(reward)
                     actions+=1
-                
-    for agent, scores in zip(agents, agent_scores):
-                
-        cumsum = np.cumsum(scores).tolist()
+
+    agent_colors = sorted(agent_scores.keys())
+    for agent, scores in agent_scores.items():
+        index = agent_colors.index(agent)
         
-        plt.plot([*range(len(scores))], cumsum, label=agent.__class__.__name__)
+        scores = [*map(lambda x: np.cumsum(x).tolist(), scores)]
+
+        avg = [sum(s)/len(s) for s in [*zip(*scores)]]
         
+        for score in scores:
+            plt.plot([*range(len(score))], score, c=plt_light_colors[index])
+
+        plt.plot([*range(len(avg))], avg, c=plt_colors[index], label=agent_labels[index])
+        
+    plt.ylabel('Score')
+    plt.xlabel('Actions')    
+    
     plt.legend()
-    plt.savefig('test.png')
+    plt.savefig('comparison.png')
     plt.show()
         
 
---
+++ rapporter/uke47.md
@@ -0,0 +1,14 @@
+# Uke 47
+
+Siste uke før innlevering.
+
+# Veileder møte
+
+
+
+# Benchmarking
+
+Lager script for å sammenligne de forskjellige agentene over n actions.
+Tester også random for å vise tydelig læring for DQN
+
+![res](./imgs/comparison.png)
